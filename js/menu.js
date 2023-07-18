591c3b484e31224dd49d7212bebb027eff26f8f8
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-07-18 15:35:37 +0300

    add cads

--- js/menu.js
+++ js/menu.js
@@ -9,17 +9,17 @@
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  refs.menuList.addEventListener("click", removeMenu);
+  // refs.menuList.addEventListener("click", removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
   }
 
-  function removeMenu() {
-    refs.menu.classList.add("is-hidden");
-    refs.body.classList.remove("no-scroll");
-  }
+  // function removeMenu() {
+  //   refs.menu.classList.add("is-hidden");
+  //   refs.body.classList.remove("no-scroll");
+  // }
 })();
 
 
