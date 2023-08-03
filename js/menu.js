a25c7cb8afe7d96f4e084b2dd44fc1d80887890f
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-08-03 21:56:02 +0300

    add button up

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,23 @@
+const btnUp = document.querySelector(".up");
+btnUp.style.display = "none";
+window.addEventListener("scroll", scrollFunction);
+btnUp.addEventListener("click", topFunction);
+
+// функція скролу
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    btnUp.style.display = "block";
+  } else {
+    btnUp.style.display = "none";
+  }
+}
+
+// функція повернення на початок сторінки
+function topFunction() {
+  console.log("ooooo");
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth" }); // Для Chrome, Firefox, IE и Opera
+}
+
 (() => {
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
@@ -22,29 +42,28 @@
   // }
 })();
 
-
 // code from Repeta
 
 // (() => {
 //     const mobileMenu = document.querySelector('.js-menu-container');
 //     const openMenuBtn = document.querySelector('.js-open-menu');
 //     const closeMenuBtn = document.querySelector('.js-close-menu');
-  
+
 //     const toggleMenu = () => {
 //       const isMenuOpen =
 //         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
 //       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
 //       mobileMenu.classList.toggle('is-open');
-  
+
 //       const scrollLockMethod = !isMenuOpen
 //         ? 'disableBodyScroll'
 //         : 'enableBodyScroll';
 //       bodyScrollLock[scrollLockMethod](document.body);
 //     };
-  
+
 //     openMenuBtn.addEventListener('click', toggleMenu);
 //     closeMenuBtn.addEventListener('click', toggleMenu);
-  
+
 //     // Close the mobile menu on wider screens if the device orientation changes
 //     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
 //       if (!e.matches) return;
