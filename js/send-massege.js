3fee26429e69345068c13139de32555a079ec93e
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-08-04 17:26:09 +0300

    add script for send message

--- js/send-massege.js
+++ js/send-massege.js
@@ -1,6 +1,10 @@
 import Notiflix from "https://cdn.jsdelivr.net/npm/notiflix@3/+esm";
+
 const formRef = document.querySelector(".online-form");
+
 const STORAGE_KEY = "feedback-form-state";
+const YOUR_SERVICE_ID = "service_ej4ym0o";
+const YOUR_TEMPLATE_ID = "template_95y95q9";
 
 formRef.addEventListener("submit", onFormSubmit);
 formRef.addEventListener("input", _.throttle(onFormInput, 500));
@@ -10,15 +14,34 @@ const FormData = {};
 const { name, phone, message } = formRef.elements;
 // під час завантаження сторінки перевіряй стан сховища
 lastSaveForm();
-console.log(name.value);
+
 function onFormSubmit(event) {
   event.preventDefault();
   // проверка на пустые поля
   if (name.value.trim() === "" || phone.value.trim() === "" || message.value.trim() === "") {
     return Notiflix.Notify.warning("All fields must be filled in");
   }
-  // виводимо у консоль об'єкт з полями email та message
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  console.log(FormData);
+
+  emailjs
+    .send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, FormData)
+    .then((response) => {
+      console.log("SUCCESS!", response.status, response.text);
+    })
+    .catch((error) => {
+      console.log("FAILED...", error);
+    });
+
+  // emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, FormData).then(
+  //   function (response) {
+  //     console.log("SUCCESS!", response.status, response.text);
+  //   },
+  //   function (error) {
+  //     console.log("FAILED...", error);
+  //   }
+  // );
+
   // очищаем поля формы
   event.currentTarget.reset();
   // очищаем localStorage
